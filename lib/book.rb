caf7f2fce51f355d6575895ec753ebb473323d79
Unsar Malik <unsar.malik24@gmail.com>
2019-05-03 01:46:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -15,5 +15,23 @@ end
 def author
   @author
 end
+
+def page_count=(num)
+  @page_count=num 
+end
+
+def page_count
+  @page_count
+end
+
+def genre=(genre)
+  @genre=genre
+end
+
+def genre
+  @genre
+end
+end
+
 end
 
